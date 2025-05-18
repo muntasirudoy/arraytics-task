a64e7dd46a5e22ec51b7cf4645cbe04c56336476
Muntasir Udoy <muntasirudoy4@gmail.com>
2025-05-18 18:29:12 +0600

    fix: icon property default true for icon-arrow primary secondary

--- problem_three/variant-config.js
+++ problem_three/variant-config.js
@@ -1,6 +1,6 @@
 export const variantStyles = new Map([
-  ["icon-arrow-primary", { bg: "blue", color: "white", icon: false }],
-  ["icon-arrow-secondary", { bg: "gray", color: "black", icon: false }],
+  ["icon-arrow-primary", { bg: "blue", color: "white", icon: true }],
+  ["icon-arrow-secondary", { bg: "gray", color: "black", icon: true }],
   ["primary", { bg: "blue", color: "white", icon: false }],
   ["secondary", { bg: "gray", color: "black", icon: false }],
 ]);
