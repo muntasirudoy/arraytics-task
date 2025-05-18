c4b9874b46c97cb153b16b9904ddeb46fc6068e9
Muntasir Udoy <muntasirudoy4@gmail.com>
2025-05-18 18:27:28 +0600

    refactor: icon property added for variant style

--- problem_three/variant-config.js
+++ problem_three/variant-config.js
@@ -1,6 +1,6 @@
 export const variantStyles = new Map([
-  ["icon-arrow-primary", { bg: "blue", color: "white", icon: true }],
-  ["icon-arrow-secondary", { bg: "gray", color: "black", icon: true }],
+  ["icon-arrow-primary", { bg: "blue", color: "white", icon: false }],
+  ["icon-arrow-secondary", { bg: "gray", color: "black", icon: false }],
   ["primary", { bg: "blue", color: "white", icon: false }],
   ["secondary", { bg: "gray", color: "black", icon: false }],
 ]);
