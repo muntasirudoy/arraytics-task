6d1be8f51bb8fb4876977d602f71db10b6c6e144
Muntasir Udoy <muntasirudoy4@gmail.com>
2025-05-18 22:48:24 +0600

    feat: add form submit function and form state, add app file with import form and product list component

--- app/components/Add-Product-Form.jsx
+++ app/components/Add-Product-Form.jsx
@@ -1,9 +1,27 @@
-const AddProductForm = () => {
-  const handleSubmit = () => {};
+const AddProductForm = ({ onAddProduct }) => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const handleSubmit = () => {
+    onAddProduct({
+      name,
+      price: parseFloat(price),
+    });
+  };
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Product Name" value={""} onChange={""} />
-      <input type="number" placeholder="Price" value={""} onChange={""} />
+      <input
+        type="text"
+        placeholder="Product Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <button type="submit">Add Product</button>
     </form>
   );
 };
---
+++ app/components/Product-List.jsx
@@ -0,0 +1,5 @@
+const ProductList = () => {
+  return <></>;
+};
+ProductList.displayName = "ProductList";
+export default ProductList;
