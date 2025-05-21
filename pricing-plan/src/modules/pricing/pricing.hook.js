9109eaebcae4d822a250777bb712afff49446e37
Muntasir Udoy <muntasirudoy4@gmail.com>
2025-05-21 15:54:08 +0600

    refactor(pricing.hook): cahnge loadData initialization process to the redux action

--- pricing-plan/src/modules/pricing/pricing.hook.js
+++ pricing-plan/src/modules/pricing/pricing.hook.js
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import planData from "./plans.data.json";
 import {
+  changeActivePlanVariation,
   loadPlanData,
   setBillingCycle,
 } from "../../store/actions/pricing-action";
-import { groupPlansByName } from "../../utils/group-plan";
+import planData from "./plans.data.json";
 
 const usePricing = () => {
   const dispatch = useDispatch();
-  const { plans, plansInfo, features, selectedCycle } = useSelector(
-    (state) => state.plan
-  );
+  const { plans, plansInfo, features, selectedCycle, activePlans } =
+    useSelector((state) => state.plan);
 
-  const [selectedVariantIndex, setSelectedVariantIndex] = useState(1);
+  // const [selectedVariantIndex, setSelectedVariantIndex] = useState(1);
 
   useEffect(() => {
     dispatch(loadPlanData(planData));
   }, [dispatch]);
 
   const handleCycleChange = (cycle) => dispatch(setBillingCycle(cycle));
-  const handleVariantChange = (val) => setSelectedVariantIndex(val);
-
-  const groupedPlans = groupPlansByName(plans);
+  const handleVariantChange = (val) => {
+    dispatch(changeActivePlanVariation(val));
+  };
 
   return {
     plansInfo,
-    groupedPlans,
+    plans,
     selectedCycle,
     features,
-    selectedVariantIndex,
+    activePlans,
+    // selectedVariantIndex,
     onCycleChange: handleCycleChange,
     onVariantChange: handleVariantChange,
   };
